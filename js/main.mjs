1390d61161444f1264aafe6adff1e0d64ab20a7b
NguyenLeMinh994 <kurosama99@gmail.com>
2019-06-19 19:19:40 +0700

    Dang lam render

--- js/main.mjs
+++ js/main.mjs
@@ -1,10 +1,15 @@
 'use strict';
+const renderPostList = (postList) => {
+  const postListElement = document.querySelector('postsList');
+};
+const postList = [];
 
 // -----------------------
 // MAIN LOGIC
 // -----------------------
 const init = async () => {
   // Write your logic here ....
+  renderPostList(todoList);
 };
 
 init();
